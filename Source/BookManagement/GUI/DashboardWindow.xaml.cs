8c0260397af8441a5bb8bdc43011050aaa09fd3f
Leo Nguyen <39960770+hoangsang160898@users.noreply.github.com>
2019-05-08 16:53:12 +0700

    dashboard + sign windows

---
+++ Source/BookManagement/GUI/DashboardWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for DashboardWindow.xaml
+    /// </summary>
+    public partial class DashboardWindow : Window
+    {
+        public DashboardWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            GridPrincipal.IsEnabled = false;
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+            GridPrincipal.IsEnabled = true;
+        }
+
+        private void MoveWindow(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void btn_CloseWindow(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_MinimizeWindow(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = ListViewMenu.SelectedIndex;
+            MoveCursorMenu(index);
+            switch (index)
+            {
+                case 0:
+                    //GridPrincipal.Content = new ViewItemsPage();
+                    break;
+                case 1:
+                    //GridPrincipal.Content = new ReportPage();
+                    break;
+                case 2:
+                    //GridPrincipal.Content = new ChartPage();
+                    break;
+                case 3:
+                    //GridPrincipal.Content = new ManageProductsPage();
+                    break;
+                case 4:
+                    //GridPrincipal.Content = new ManageSalesPage();
+                    break;
+                case 5:
+                    //GridPrincipal.Content = new AboutPage();
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        private void MoveCursorMenu(int index)
+        {
+            GridCursor.Margin = new Thickness(0, (148+(50 * index)), 0, 0);
+
+        }
+    }
+}
